6eaa6cebc00becebcc5545d7ed637ce5ab25ba09
lucas874 <lsc500@outlook.dk>
2026-03-25 14:53:41 +0100

    Try timeout

--- swarm/src/spawner_silent_multiple_machines_per_process.ts
+++ swarm/src/spawner_silent_multiple_machines_per_process.ts
@@ -32,7 +32,7 @@ async function main() {
     const appId = `com.example.${displayName}`
     const first_half_a = "dist/machine_drivers/run_first_half_A.js"
     const second_half_a = "dist/machine_drivers/run_second_half_A.js"
-    const first_half_b = "dist/machine_drivers/run_first_half_B.js"
+    const first_half_b = "dist/machine_drivers/run_first_half_B_no_steel_t.js"
     const second_half_b = "dist/machine_drivers/run_second_half_B.js"
     const commands = argv.choiceAorB === "a" ? [[first_half_a], [second_half_a]]
         : argv.choiceAorB === "b" ? [[first_half_b], [second_half_b]] : undefined
@@ -44,13 +44,13 @@ async function main() {
     const transporterCommand = argv.choiceAorB === "a" ? "dist/machine_drivers/run_two_transporters_A.js"
         : argv.choiceAorB === "b" ? "dist/machine_drivers/run_two_transporters_B.js" : undefined
     if (!transporterCommand) { throw Error(`Invalid argument ${argv.choiceAorB} given`)}
-    //const steelTransportCommand = "dist/machine_drivers/run_steel_transport_B.js"
+    const steelTransportCommand = "dist/machine_drivers/run_steel_transport_B.js"
 
     const processes: ReturnType<typeof execa>[] = [];
 
     // Each time we execute a command we start 7 machines
     const quotient = Math.floor(N / 7)
-    const totalNumProcesses = quotient + 1;
+    const totalNumProcesses = quotient + 2;
     let machinesSpawned = 0;
     // Spawn processes
     for (let i = 0; i < quotient; i++) {
@@ -76,7 +76,7 @@ async function main() {
         stderr: "ignore",
     }));
     machinesSpawned = machinesSpawned + 2
-    clearLineAndPrint(`Spawned processes: ${totalNumProcesses}/${totalNumProcesses}. Total number of machines spawned: ${machinesSpawned}`)
+    clearLineAndPrint(`Spawned processes: ${totalNumProcesses-1}/${totalNumProcesses}. Total number of machines spawned: ${machinesSpawned}`)
 
     /* processes.push(execa(`node`, [steelTransportCommand], {
         stdout: "ignore",
@@ -84,6 +84,16 @@ async function main() {
     }));
     machinesSpawned = machinesSpawned + 1
     clearLineAndPrint(`Spawned processes: ${totalNumProcesses}/${totalNumProcesses}. Total number of machines spawned: ${machinesSpawned}`) */
+    setTimeout(() => {
+        processes.push(execa(`node`, [steelTransportCommand], {
+            stdout: "ignore",
+            stderr: "ignore",
+        }));
+        machinesSpawned = machinesSpawned + 1
+        clearLineAndPrint(`Spawned processes: ${totalNumProcesses}/${totalNumProcesses}. Total number of machines spawned: ${machinesSpawned}`)
+        console.log()
+    },  4000)
+
 
     console.log()
     // Update termination spinner as processes exit
