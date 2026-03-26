0fc235b49dec61890d59e279dc743e81c760843b
lucas874 <lsc500@outlook.dk>
2026-03-26 11:48:33 +0100

    Cleaning.

--- swarm/src/machine_drivers/run_steel_transport_B.ts
+++
@@ -1,8 +0,0 @@
-import { main as steelTransportMain } from "./steel_press_protocol/steel_transport.js";
-import { runAsyncFunctionsB } from "./call_async_functions.js";
-
-const mainFunctions = [
-    steelTransportMain,
-]
-
-runAsyncFunctionsB(mainFunctions)
